6866c8ed03200a0c587004789563cc5195e962cf
GustavoAntonio12 <gustavo.antonio@sptech.school>
2022-08-20 22:38:21 -0300

    Estruturando API

---
+++ site_institucional/src/database/script-tabelas.sql
@@ -0,0 +1,7 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+/* para workbench - local - desenvolvimento */
+CREATE DATABASE airData;
+
+USE airData;
