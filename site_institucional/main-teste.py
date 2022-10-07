1c6bd5c0bd27a2be7dc4843f143b10cae5119757
ryan-miyazato <ryan.miyazato@sptech.school>
2022-10-07 20:01:24 -0300

    add: API python de captura de dados provisória em /site-institucional

---
+++ site_institucional/main-teste.py
@@ -0,0 +1,32 @@
+import psutil as ps
+import mysql.connector 
+import os
+import datetime
+from time import sleep
+
+bdsql = mysql.connector.connect(user='airdata_client', password='sptech', host='localhost', database='airData')
+cursor = bdsql.cursor()
+
+while True:
+    cpuPercent = ps.cpu_percent(interval=0.1)
+    ramPercent = ps.virtual_memory().percent
+    diskPercent = ps.disk_usage("/").percent
+    
+    comando = f'INSERT INTO leitura (fkMetrica, horario, valorLido, fkComponente_idComponente, fkComponente_fkServidor) VALUES (1, now(), {cpuPercent}, 1, 1);'
+
+    cursor.execute(comando)
+    bdsql.commit()
+
+    comando = f'INSERT INTO leitura (fkMetrica, horario, valorLido, fkComponente_idComponente, fkComponente_fkServidor) VALUES (2, now(), {ramPercent}, 1, 1);'
+
+    cursor.execute(comando)
+    bdsql.commit()
+
+    comando = f'INSERT INTO leitura (fkMetrica, horario, valorLido, fkComponente_idComponente, fkComponente_fkServidor) VALUES (3, now(), {diskPercent}, 1, 1);'
+
+    cursor.execute(comando)
+    bdsql.commit()
+    
+    print(cpuPercent, ramPercent, diskPercent)
+    
+    sleep(1)
