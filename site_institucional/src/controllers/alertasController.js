00334af0d8caaf344f2d3d5a129dc5b1d0c55ee1
KauanCavazani <Kauan.cavazani@gmail.com>
2022-10-16 11:30:36 -0300

    fix: bug do filtro de alertas

--- site_institucional/src/controllers/alertasController.js
+++ site_institucional/src/controllers/alertasController.js
@@ -59,12 +59,24 @@ function receberDadosFiltro(req, res) {
         res.status(400).send("A máquina está undefined!");
     } else if (hardware == 'null' && maquina == 'null') {
         alertasModel.listarAlertas(fkTorre, 10)
+            .then(
+                function (resultado) {
+                    console.log(`\nAlertas: ${resultado}`);
+                    res.json(false);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao receber os alertas! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            )
     } else {
         alertasModel.listarAlertasFiltrados(fkTorre, limite, hardware, maquina)
             .then(
                 function (resultado) {
                     console.log(`\nAlertas: ${resultado}`);
-                    res.json(resultado)
+                    res.json(resultado);
                 }
             ).catch(
                 function (erro) {
