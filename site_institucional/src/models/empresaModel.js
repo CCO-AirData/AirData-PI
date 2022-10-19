8ae162fc80a0b4380884543189e189468d968ebf
HugoHanashiro <hugo.galdino@sptech.school>
2022-10-18 22:15:40 -0300

    arrumando bugs no cadastro causados pela migração p azure

--- site_institucional/src/models/empresaModel.js
+++ site_institucional/src/models/empresaModel.js
@@ -1,13 +1,14 @@
 var database = require("../database/config")
 
 function cadastrar(nomeEmpresa,cnpjEmpresa,telefoneEmpresa) {
+    let instrucao = "";
     if(process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-
+        instrucao = `INSERT INTO empresa (nomeEmpresa,cnpjEmpresa,telefoneEmpresa) VALUES ('${nomeEmpresa}', '${cnpjEmpresa}', '${telefoneEmpresa}');`;
     } else if(process.env.AMBIENTE_PROCESSO == "producao") {
-        var instrucao = `INSERT INTO empresa (nomeEmpresa,cnpjEmpresa,telefoneEmpresa) VALUES ('${nomeEmpresa}', '${cnpjEmpresa}', '${telefoneEmpresa}');
-                        SELECT @@IDENTITY AS ID;`;
-        console.log("Executando a instrução SQL: \n" + instrucao);    
+        instrucao = `INSERT INTO empresa (nomeEmpresa,cnpjEmpresa,telefoneEmpresa) VALUES ('${nomeEmpresa}', '${cnpjEmpresa}', '${telefoneEmpresa}');
+                        SELECT @@IDENTITY AS ID;`; 
     }
+    console.log("Executando a instrução SQL: \n" + instrucao);   
     return database.executar(instrucao);
 }
 
