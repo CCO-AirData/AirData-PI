c3c3a287c6a7a9d0b363953ce0d825fff7a02c49
KauanCavazani <kauan.cavazani@gmail.com>
2022-11-30 19:21:02 -0300

    fix: select do gráfico de análise

--- site_institucional/src/models/medidasModel.js
+++ site_institucional/src/models/medidasModel.js
@@ -25,13 +25,8 @@ function medidasCardsTempoReal(idMaquina, metrica, nomeComponente, nomeMetrica, 
 }
 
 function medidasGraficoTempoReal(idMaquina, metrica, idComponente, idMetrica, mes) {
-    if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        var instrucao = `SELECT * FROM vw_${metrica} WHERE idServidor = '${idMaquina}' AND idComponente = ${idComponente} AND idMetrica = ${idMetrica} AND Mes = '${mes}';`;
-        console.log("Executando a instrução SQL: \n" + instrucao);
-    } else if (process.env.AMBIENTE_PROCESSO == "producao") {
-        var instrucao = `SELECT TOP ${limite} * FROM vw_${metrica} WHERE idServidor = '${idMaquina}' AND idComponente = ${idComponente};`;
-        console.log("Executando a instrução SQL: \n" + instrucao);
-    }
+    var instrucao = `SELECT * FROM vw_${metrica} WHERE idServidor = '${idMaquina}' AND idComponente = ${idComponente} AND idMetrica = ${idMetrica} AND Mes = '${mes}';`;
+    console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
